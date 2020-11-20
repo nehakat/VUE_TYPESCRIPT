9e101dec2a66d8965b7a2c47c3f0339d05aacdde
Neha Katariya <katneha.70@gmail.com>
2020-11-20 14:04:51 +0530

    first commit

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,14 +1,14 @@
 import Vue from 'vue'
 import VueRouter, { RouteConfig } from 'vue-router'
-import Home from '../views/Home.vue'
+import MyInvestments from '../views/MyInvestments.vue'
 
 Vue.use(VueRouter)
 
 const routes: Array<RouteConfig> = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'My Investments',
+    component: MyInvestments
   },
   {
     path: '/about',
---
+++ src/views/MyInvestments.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    This is my invessment dashboard view
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'MyInvestments',
+  components: {
+  }
+}
+</script>
